7e3e9762f75279e7a62e53e9d05eae3867dc82bb
Simon Walker <s.walker-12@student.lboro.ac.uk>
2015-03-21 19:19:17 +0000

    Added slide up and hide animation

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,4 +1,11 @@
 'use strict';
 app.controller('indexController', ['$scope', function ($scope) {
+
     
 }]);
+
+$( window ).scroll(function() {
+  console.log('Event Fired');
+
+  $(".title_cards").slideUp(500);
+});
