1562e8fa85fc556a6221d0016658558c1ebfe91f
Simon Walker <s.walker-12@student.lboro.ac.uk>
2015-03-21 18:33:33 +0000

    Added title-card carousel
    todo: add scroll animation

--- app/app.js
+++ app/app.js
@@ -1,6 +1,29 @@
 
 var app = angular.module('filmApp', ['ngRoute']);
 
+app.directive("scroll", function ($window) {
+    return function(scope, $element, attrs) {
+        angular.element($window).bind("scroll", function() {
+           
+            scope.boolChangeClass = true;
+
+            scope.$apply();
+        });
+    };
+});
+
+
+app.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm) {
+      $elm.on('click', function() {
+        $("body").animate({scrollTop: $elm.offset().top}, "slow");
+      });
+    }
+  }
+});
+
 app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
@@ -11,5 +34,4 @@ app.config(function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: "/home" });
     
 });
-
 
--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,3 +1,4 @@
 'use strict';
 app.controller('homeController', ['$scope', function ($scope) {
+		$scope.names = ["simon","jaeren", "james"];
 }]);
