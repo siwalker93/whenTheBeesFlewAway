f61741577af67d1b84109e7a8089ae8283c8bcdf
JaerenCoathup <Jaeren@Jaerens-MacBook-Pro.local>
2015-03-21 18:50:04 +0000

    adding content and nav bar

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,4 +1,12 @@
 'use strict';
 app.controller('homeController', ['$scope', function ($scope) {
-		$scope.names = ["simon","jaeren", "james"];
+    $scope.currentTab = 'home';
+    
+    $scope.changeTab = function(tabName){
+        $scope.currentTab = tabName;
+    }
+    
+    $scope.isCurrentTab = function(tabName){
+        return $scope.currentTab == tabName;
+    }
 }]);
