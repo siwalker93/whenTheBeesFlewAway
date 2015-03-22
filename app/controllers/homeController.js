e9bdb3df14cc87a0556bbb536ce557fbffb1f56c
JaerenCoathup <Jaeren@Jaerens-MacBook-Pro.local>
2015-03-22 18:44:52 +0000

    Added images for nabber and centre page

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,6 +1,28 @@
 'use strict';
 app.controller('homeController', ['$scope', function ($scope) {
+    //Sets start tab
     $scope.currentTab = 'home';
+    
+    //ng-src = "{{backgroundImagePaths[currentTab]}}"
+    
+    //What background image should be shown for each tab
+    var backgroundImagePaths = {};
+    backgroundImagePaths["home"] = "content/img/backgrounds/paper-texture.png";
+    backgroundImagePaths["project"] = "content/img/backgrounds/project-background-1170.png";
+    backgroundImagePaths["film"] = "content/img/backgrounds/paper-texture.png";
+    backgroundImagePaths["people"] = "content/img/backgrounds/paper-texture.png";
+    backgroundImagePaths["bees"] = "content/img/backgrounds/paper-texture.png";
+    backgroundImagePaths["pledge"] = "content/img/backgrounds/paper-texture.png";
+    
+    var headerImagePaths = {};
+    headerImagePaths["home"] = "content/img/headers/project-header-1170.png";
+    headerImagePaths["project"] = "content/img/headers/project-header-1170.png";
+    headerImagePaths["film"] = "content/img/headers/project-header-1170.png";
+    headerImagePaths["people"] = "content/img/headers/project-header-1170.png";
+    headerImagePaths["bees"] = "content/img/headers/project-header-1170.png";
+    headerImagePaths["pledge"] = "content/img/headers/project-header-1170.png";
+    
+    
     
     $scope.changeTab = function(tabName){
         $scope.currentTab = tabName;
@@ -8,5 +30,11 @@ app.controller('homeController', ['$scope', function ($scope) {
     
     $scope.isCurrentTab = function(tabName){
         return $scope.currentTab == tabName;
+    }
+    
+    $scope.getBackgroundImage = function(){
+        var imagePath = backgroundImagePaths[$scope.currentTab];
+        return {'background-image': 'url("' + imagePath + '")',
+                'background-rep': 'no-repeat'};
     }
 }]);
--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -5,7 +5,5 @@ app.controller('indexController', ['$scope', function ($scope) {
 }]);
 
 $( window ).scroll(function() {
-  console.log('Event Fired');
-
   $(".title_cards").slideUp(500);
 });
