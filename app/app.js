e10deb227723db86331460d48c91affb716720f6
Simon Walker <s.walker-12@student.lboro.ac.uk>
2015-05-17 23:27:59 +0100

    Finished title cards and adjusted general css

--- app/app.js
+++ app/app.js
@@ -12,3 +12,26 @@ app.config(function ($routeProvider) {
     
 });
 
+app.directive('resize', function ($window) {
+	return function (scope, element) {
+		var w = angular.element($window);
+		scope.$watch(function () {
+			return { 'h': w.height(), 'w': w.width() };
+		}, function (newValue, oldValue) {
+			scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+            
+            scope.style = function () {
+				return { 
+                    'height': (newValue.h - 100) + 'px',
+                    'width': (newValue.w - 100) + 'px' 
+                };
+			};
+            
+		}, true);
+	
+		w.bind('resize', function () {
+			scope.$apply();
+		});
+	}
+})
--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,7 +1,10 @@
 'use strict';
 app.controller('indexController', ['$scope', function ($scope) {
+	$('.small_display_splash.h1').css('height', $( window ).innerHeight());
 
-    
+	$scope.popupHeight = $window.innerHeight;
+	console.log($scope.popupHeight);
+  
 }]);
 
 $( window ).scroll(function() {
